8e8d8001a0d50bec1eb39269312f70b8348cd7cd
Landrey Smith <smithlandrey@gmail.com>
2025-08-11 17:33:53 -0700

    chore(terraform): fmt

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -1,10 +1,10 @@
-variable "db_username" { 
-    type = string 
+variable "db_username" {
+  type = string
 }
 
-variable "db_password" { 
-    type = string
-    sensitive = true 
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
 
 
